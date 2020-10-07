3d857d3ca879386d4ca21088dd70fdae2aaf546e
Yuta Koshizawa <koher@koherent.org>
2020-10-08 01:44:28 +0900

    Improve performance of `permutations(of:)`
    From 0.120s to 0.049s for `permutations(of: Array(1 ... 8)`.

--- Sources/AtCoderSupport/Permutations.swift
+++ Sources/AtCoderSupport/Permutations.swift
@@ -1,48 +1,19 @@
-func permutations<C>(of values: C) -> [[C.Element]] where C: RandomAccessCollection, C.Indices == Range<Int>, C.SubSequence: RangeReplaceableCollection, C.SubSequence.Indices == Range<Int> {
-    if values.isEmpty { return [[]] }
-    if values.count == 1 { return [[values[0]]] }
-    if values.count == 2 { return [[values[1], values[0]], [values[0], values[1]]] }
-    if values.count == 3 { return [
-        [values[2], values[1], values[0]],
-        [values[2], values[0], values[1]],
-        [values[1], values[2], values[0]],
-        [values[1], values[0], values[2]],
-        [values[0], values[2], values[1]],
-        [values[0], values[1], values[2]],
-    ] }
-    if values.count == 4 { return [
-        [values[3], values[2], values[1], values[0]],
-        [values[3], values[2], values[0], values[1]],
-        [values[3], values[1], values[2], values[0]],
-        [values[3], values[1], values[0], values[2]],
-        [values[3], values[0], values[2], values[1]],
-        [values[3], values[0], values[1], values[2]],
-        [values[2], values[3], values[1], values[0]],
-        [values[2], values[3], values[0], values[1]],
-        [values[2], values[1], values[3], values[0]],
-        [values[2], values[1], values[0], values[3]],
-        [values[2], values[0], values[3], values[1]],
-        [values[2], values[0], values[1], values[3]],
-        [values[1], values[3], values[2], values[0]],
-        [values[1], values[3], values[0], values[2]],
-        [values[1], values[2], values[3], values[0]],
-        [values[1], values[2], values[0], values[3]],
-        [values[1], values[0], values[3], values[2]],
-        [values[1], values[0], values[2], values[3]],
-        [values[0], values[3], values[2], values[1]],
-        [values[0], values[3], values[1], values[2]],
-        [values[0], values[2], values[3], values[1]],
-        [values[0], values[2], values[1], values[3]],
-        [values[0], values[1], values[3], values[2]],
-        [values[0], values[1], values[2], values[3]],
-    ] }
+func permutations<C>(of values: C) -> [[C.Element]]
+        where C: RandomAccessCollection, C.SubSequence: RangeReplaceableCollection,
+              C.Indices == Range<Int>, C.SubSequence.Indices == Range<Int> {
     var result: [[C.Element]] = []
-    for i in values.indices {
-        var subresult = permutations(of: values[..<i] + values[(i + 1)...])
-        for j in subresult.indices {
-            subresult[j].append(values[i])
-        }
-        result.append(contentsOf: subresult)
-    }
+    _permutations(of: Array(values), indices: values.indices, result: &result)
     return result
+}
+
+private func _permutations<T>(of values: [T], indices: Range<Int>, result: inout [[T]]) {
+    if indices.isEmpty {
+        result.append(values)
+        return
+    }
+    var values = values
+    for i in indices {
+        values.swapAt(indices.lowerBound, i)
+        _permutations(of: values, indices: (indices.lowerBound + 1) ..< indices.upperBound, result: &result)
+    }
 }
