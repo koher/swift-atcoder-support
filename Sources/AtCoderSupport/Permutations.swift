33a7346b3e8bce24d8a1cf9335338243e5c15ad6
Yuta Koshizawa <koher@koherent.org>
2020-10-08 23:22:30 +0900

    Change `permutations` to method

--- Sources/AtCoderSupport/Permutations.swift
+++ Sources/AtCoderSupport/Permutations.swift
@@ -1,18 +1,20 @@
-func permutations<S: Sequence>(of values: S) -> [[S.Element]] {
-    func _permutations<T>(of values: [T], indices: Range<Int>, result: inout [[T]]) {
-        if indices.isEmpty {
-            result.append(values)
-            return
+extension Sequence {
+    func permutations() -> [[Element]] {
+        func _permutations<T>(of values: [T], indices: Range<Int>, result: inout [[T]]) {
+            if indices.isEmpty {
+                result.append(values)
+                return
+            }
+            var values = values
+            for i in indices {
+                values.swapAt(indices.lowerBound, i)
+                _permutations(of: values, indices: (indices.lowerBound + 1) ..< indices.upperBound, result: &result)
+            }
         }
-        var values = values
-        for i in indices {
-            values.swapAt(indices.lowerBound, i)
-            _permutations(of: values, indices: (indices.lowerBound + 1) ..< indices.upperBound, result: &result)
-        }
+        
+        var result: [[Element]] = []
+        let values = Array(self)
+        _permutations(of: values, indices: values.indices, result: &result)
+        return result
     }
-    
-    var result: [[S.Element]] = []
-    let values = Array(values)
-    _permutations(of: values, indices: values.indices, result: &result)
-    return result
 }
--- Tests/AtCoderSupportTests/PermutationsTests.swift
+++ Tests/AtCoderSupportTests/PermutationsTests.swift
@@ -4,23 +4,23 @@ import XCTest
 final class PermutationsTests: XCTestCase {
     func testPermutations() {
         do {
-            let r = Set(permutations(of: [] as [Int]))
+            let r = Set([Int]().permutations())
             XCTAssertEqual(r.count, 1)
             XCTAssertTrue(r.contains([]))
         }
         do {
-            let r = Set(permutations(of: [1]))
+            let r = Set([1].permutations())
             XCTAssertEqual(r.count, 1)
             XCTAssertTrue(r.contains([1]))
         }
         do {
-            let r = Set(permutations(of: [1, 2]))
+            let r = Set([1, 2].permutations())
             XCTAssertEqual(r.count, 2)
             XCTAssertTrue(r.contains([1, 2]))
             XCTAssertTrue(r.contains([2, 1]))
         }
         do {
-            let r = Set(permutations(of: [1, 2, 3]))
+            let r = Set([1, 2, 3].permutations())
             XCTAssertEqual(r.count, 6)
             XCTAssertTrue(r.contains([1, 2, 3]))
             XCTAssertTrue(r.contains([1, 3, 2]))
@@ -30,7 +30,7 @@ final class PermutationsTests: XCTestCase {
             XCTAssertTrue(r.contains([3, 2, 1]))
         }
         do {
-            let r = Set(permutations(of: [1, 2, 3, 4]))
+            let r = Set([1, 2, 3, 4].permutations())
             XCTAssertEqual(r.count, 24)
             XCTAssertTrue(r.contains([1, 2, 3, 4]))
             XCTAssertTrue(r.contains([1, 2, 4, 3]))
@@ -57,8 +57,8 @@ final class PermutationsTests: XCTestCase {
             XCTAssertTrue(r.contains([4, 3, 1, 2]))
             XCTAssertTrue(r.contains([4, 3, 2, 1]))
         }
-        do { // Collection of `Character`s
-            let r = Set(permutations(of: Array("ABC")))
+        do { // `Sequence`s of `Character`s
+            let r = Set("ABC".permutations())
             XCTAssertEqual(r.count, 6)
             XCTAssertTrue(r.contains(["A", "B", "C"]))
             XCTAssertTrue(r.contains(["A", "C", "B"]))
@@ -68,7 +68,7 @@ final class PermutationsTests: XCTestCase {
             XCTAssertTrue(r.contains(["C", "B", "A"]))
         }
         do { // Reverse order inputs
-            let r = Set(permutations(of: [3, 2, 1]))
+            let r = Set([3, 2, 1].permutations())
             XCTAssertEqual(r.count, 6)
             XCTAssertTrue(r.contains([1, 2, 3]))
             XCTAssertTrue(r.contains([1, 3, 2]))
@@ -78,7 +78,7 @@ final class PermutationsTests: XCTestCase {
             XCTAssertTrue(r.contains([3, 2, 1]))
         }
         do { // Collections but `Array`s
-            let r = Set(permutations(of: [0, 1, 2, 3, 4][1 ..< 4]))
+            let r = Set([0, 1, 2, 3, 4][1 ..< 4].permutations())
             XCTAssertEqual(r.count, 6)
             XCTAssertTrue(r.contains([1, 2, 3]))
             XCTAssertTrue(r.contains([1, 3, 2]))
@@ -91,7 +91,7 @@ final class PermutationsTests: XCTestCase {
     
     func testPermutatinsPerformance() {
         measure {
-            let r = permutations(of: Array(1 ... 8))
+            let r = (1 ... 8).permutations()
             XCTAssertEqual(r.count, 40320)
         }
     }
