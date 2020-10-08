54bb4c51f0f116b8c866e8aae254d4295f8d2fd7
Yuta Koshizawa <koher@koherent.org>
2020-10-09 02:00:36 +0900

    Modify APIs of `bfs` to make them more useful

--- Sources/AtCoderSupport/BFS.swift
+++ Sources/AtCoderSupport/BFS.swift
@@ -1,13 +1,12 @@
-func bfs<Node>(nodes: [Node], edges: [[Int]], startedAt start: Int, _ operation: (Node) -> Void) {
-    precondition(nodes.count == edges.count)
-    precondition(nodes.indices.contains(start))
+func bfs(edges: [[Int]], startedAt start: Int, _ operation: (Int) -> Void) {
+    precondition(edges.indices.contains(start))
     var visited: Set<Int> = []
     var nexts: [Int] = [start]
     while !nexts.isEmpty {
         var destinations: [Int] = []
         for current in nexts {
             if visited.contains(current) { continue }
-            operation(nodes[current])
+            operation(current)
             visited.insert(current)
             for destination in edges[current] {
                 destinations.append(destination)
@@ -17,19 +16,36 @@ func bfs<Node>(nodes: [Node], edges: [[Int]], startedAt start: Int, _ operation:
     }
 }
 
-func bfs<Node>(nodes: inout [Node], edges: [[Int]], startedAt start: Int, _ operation: (inout Node) -> Void) {
-    precondition(nodes.count == edges.count)
-    precondition(nodes.indices.contains(start))
+func bfs(edges: [[Int]], startedAt start: Int, _ operation: (_ current: Int, _ prev: Int?) -> Void) {
+    precondition(edges.indices.contains(start))
     var visited: Set<Int> = []
-    var nexts: [Int] = [start]
+    var nexts: [(current: Int, prev: Int?)] = [(start, nil)]
     while !nexts.isEmpty {
-        var destinations: [Int] = []
-        for current in nexts {
+        var destinations: [(current: Int, prev: Int?)] = []
+        for (current, prev) in nexts {
             if visited.contains(current) { continue }
-            operation(&nodes[current])
+            operation(current, prev)
             visited.insert(current)
             for destination in edges[current] {
-                destinations.append(destination)
+                destinations.append((destination, current))
+            }
+        }
+        nexts = destinations
+    }
+}
+
+func bfs(edges: [[Int]], startedAt start: Int, _ operation: (_ current: Int, _ prev: Int?, _ depth: Int) -> Void) {
+    precondition(edges.indices.contains(start))
+    var visited: Set<Int> = []
+    var nexts: [(current: Int, prev: Int?, depth: Int)] = [(start, nil, 0)]
+    while !nexts.isEmpty {
+        var destinations: [(current: Int, prev: Int?, depth: Int)] = []
+        for (current, prev, depth) in nexts {
+            if visited.contains(current) { continue }
+            operation(current, prev, depth)
+            visited.insert(current)
+            for destination in edges[current] {
+                destinations.append((destination, current, depth + 1))
             }
         }
         nexts = destinations
