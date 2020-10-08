5ee6781f0a4867933b62bcfe5d383326ef9817b0
Yuta Koshizawa <koher@koherent.org>
2020-10-08 09:38:42 +0900

    Change `_permutations` to nested function
    Because it is harder to make a mistake when we copy and paste the function.

--- Sources/AtCoderSupport/Permutations.swift
+++ Sources/AtCoderSupport/Permutations.swift
@@ -1,18 +1,18 @@
 func permutations<S: Sequence>(of values: S) -> [[S.Element]] {
+    func _permutations<T>(of values: [T], indices: Range<Int>, result: inout [[T]]) {
+        if indices.isEmpty {
+            result.append(values)
+            return
+        }
+        var values = values
+        for i in indices {
+            values.swapAt(indices.lowerBound, i)
+            _permutations(of: values, indices: (indices.lowerBound + 1) ..< indices.upperBound, result: &result)
+        }
+    }
+    
     var result: [[S.Element]] = []
     let values = Array(values)
     _permutations(of: values, indices: values.indices, result: &result)
     return result
-}
-
-private func _permutations<T>(of values: [T], indices: Range<Int>, result: inout [[T]]) {
-    if indices.isEmpty {
-        result.append(values)
-        return
-    }
-    var values = values
-    for i in indices {
-        values.swapAt(indices.lowerBound, i)
-        _permutations(of: values, indices: (indices.lowerBound + 1) ..< indices.upperBound, result: &result)
-    }
 }
