30d23f74ad5d03700fa030df36482d83685c14bb
Yuta Koshizawa <koher@koherent.org>
2020-10-16 01:05:55 +0900

    Improve performance of `primes(upTo:)`

--- Sources/AtCoderSupport/Primes.swift
+++ Sources/AtCoderSupport/Primes.swift
@@ -2,7 +2,7 @@ func primes(upTo number: Int) -> [Int] {
     precondition(number >= 0)
     if number < 2 { return [] }
     var sieve: [Bool] = .init(repeating: false, count: number + 1)
-    for m in 2 ... Int(Double(number).squareRoot() + 1.5) {
+    for m in stride(from: 3, through: Int(Double(number).squareRoot() + 1.5), by: 2) {
         if sieve[m] { continue }
         for k in stride(from: m * 2, through: number, by: m) {
             sieve[k] = true
