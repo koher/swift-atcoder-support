cf0b7150f958223a54055e1b9c0cb65336379a62
Yuta Koshizawa <koher@koherent.org>
2020-10-16 03:51:08 +0900

    Implement `Deque`

---
+++ Tests/AtCoderSupportTests/DequeTests.swift
@@ -0,0 +1,54 @@
+import XCTest
+@testable import AtCoderSupport
+
+final class DequeTests: XCTestCase {
+    func testAppend() {
+        var deque: Deque<Int> = []
+        for n in 1 ... 100 {
+            deque.append(n)
+            XCTAssertEqual(deque.count, n)
+            XCTAssertEqual(deque[0], 1)
+            XCTAssertEqual(deque[n - 1], n)
+            XCTAssertEqual(Array(deque), Array(1 ... n))
+        }
+    }
+    
+    func testPopLast() {
+        var deque: Deque<Int> = .init(1 ... 100)
+        var n = 100
+        while let last = deque.popLast() {
+            XCTAssertEqual(last, n)
+            n -= 1
+            if deque.isEmpty { continue }
+            XCTAssertEqual(deque.count, n)
+            XCTAssertEqual(deque[0], 1)
+            XCTAssertEqual(deque[n - 1], n)
+            XCTAssertEqual(Array(deque), Array(1 ... n))
+        }
+    }
+
+    func testAppendFirst() {
+        var deque: Deque<Int> = []
+        for n in 1 ... 100 {
+            deque.appendFirst(n)
+            XCTAssertEqual(deque.count, n)
+            XCTAssertEqual(deque[0], n)
+            XCTAssertEqual(deque[n - 1], 1)
+            XCTAssertEqual(Array(deque), Array((1 ... n).reversed()))
+        }
+    }
+    
+    func testPopFirst() {
+        var deque: Deque<Int> = .init((1 ... 100).reversed())
+        var n = 100
+        while let first = deque.popFirst() {
+            XCTAssertEqual(first, n)
+            n -= 1
+            if deque.isEmpty { continue }
+            XCTAssertEqual(deque.count, n)
+            XCTAssertEqual(deque[0], n)
+            XCTAssertEqual(deque[n - 1], 1)
+            XCTAssertEqual(Array(deque), Array((1 ... n).reversed()))
+        }
+    }
+}
