6f17c306a339ed4f4c9234f8ca5052ee26a60558
Yuta Koshizawa <koher@koherent.org>
2020-10-18 04:37:04 +0900

    Improve performance of `tsp(distances:startedAt:)`

--- Sources/AtCoderSupport/TSP.swift
+++ Sources/AtCoderSupport/TSP.swift
@@ -1,7 +1,9 @@
 func tsp<Distance>(distances: [[Distance]], startedAt start: Int) -> Distance where Distance: Comparable, Distance: AdditiveArithmetic {
     precondition(!distances.isEmpty)
-    var results: [[Distance?]] = (0 ..< (1 << distances.count)).map { _ in [Distance?](repeating: nil, count: distances.count) }
-    results[(1 << distances.count) - 1] = distances.indices.map { distances[$0][start] }
+    var results: [Distance?] = .init(repeating: nil, count: (1 << distances.count) * distances.count)
+    for position in distances.indices {
+        results[((1 << distances.count) - 1) * distances.count + position] = distances[position][start]
+    }
     for visited in (0 ..< (1 << distances.count) - 1).reversed() {
         if visited & (1 << start) == 0 { continue }
         for position in distances.indices {
@@ -10,11 +12,11 @@ func tsp<Distance>(distances: [[Distance]], startedAt start: Int) -> Distance wh
             for next in distances.indices {
                 if visited & (1 << next) != 0 { continue }
                 let distanceToNext: Distance = distances[position][next]
-                let distance = distanceToNext + results[visited | (1 << next)][next]!
+                let distance = distanceToNext + results[(visited | (1 << next)) * distances.count + next]!
                 minDistance = minDistance.map { Swift.min($0, distance) } ?? distance
             }
-            results[visited][position] = minDistance!
+            results[visited * distances.count + position] = minDistance!
         }
     }
-    return results[1 << start][start]!
+    return results[(1 << start) * distances.count + start]!
 }
