775922df38f80fa2ec3782e44885b20bbf571598
Yuta Koshizawa <koher@koherent.org>
2020-10-18 04:28:06 +0900

    Improve performance of `tsp(distances:startedAt)`

--- Sources/AtCoderSupport/TSP.swift
+++ Sources/AtCoderSupport/TSP.swift
@@ -1,23 +1,20 @@
 func tsp<Distance>(distances: [[Distance]], startedAt start: Int) -> Distance where Distance: Comparable, Distance: AdditiveArithmetic {
     precondition(!distances.isEmpty)
-    var tspResults: [[Distance?]] = distances.indices.map { _ in [Distance?](repeating: nil, count: 1 << distances.count) }
-    func _tsp(position: Int, visited: Int) -> Distance {
-        if let result = tspResults[position][visited] { return result }
-        if visited == (1 << distances.count) - 1 {
-            let result = distances[position][start]
-            tspResults[position][visited] = result
-            return result
+    var results: [[Distance?]] = (0 ..< (1 << distances.count)).map { _ in [Distance?](repeating: nil, count: distances.count) }
+    results[(1 << distances.count) - 1] = distances.indices.map { distances[$0][start] }
+    for visited in (0 ..< (1 << distances.count) - 1).reversed() {
+        if visited & (1 << start) == 0 { continue }
+        for position in distances.indices {
+            var minDistance: Distance?
+            if visited & (1 << position) == 0 { continue }
+            for next in distances.indices {
+                if visited & (1 << next) != 0 { continue }
+                let distanceToNext: Distance = distances[position][next]
+                let distance = distanceToNext + results[visited | (1 << next)][next]!
+                minDistance = minDistance.map { Swift.min($0, distance) } ?? distance
+            }
+            results[visited][position] = minDistance!
         }
-        var minDistance: Distance?
-        for next in distances.indices {
-            if visited & (1 << next) != 0 { continue }
-            let distanceToNext: Distance = distances[position][next]
-            let distance = distanceToNext + _tsp(position: next, visited: visited | (1 << next))
-            minDistance = minDistance.map { Swift.min($0, distance) } ?? distance
-        }
-        let result =  minDistance!
-        tspResults[position][visited] = result
-        return result
     }
-    return _tsp(position: start, visited: 1 << start)
+    return results[1 << start][start]!
 }
