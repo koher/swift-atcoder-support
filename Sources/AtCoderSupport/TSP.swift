0485f4b8939a196a985aaa0f90aadebd16470511
Yuta Koshizawa <koher@koherent.org>
2020-10-18 03:19:27 +0900

    Improve performance of `tsp(distances:startedAt:)`

--- Sources/AtCoderSupport/TSP.swift
+++ Sources/AtCoderSupport/TSP.swift
@@ -1,7 +1,13 @@
 func tsp<Distance>(distances: [[Distance]], startedAt start: Int) -> Distance where Distance: Comparable, Distance: AdditiveArithmetic {
     precondition(!distances.isEmpty)
+    var tspResults: [[Distance?]] = distances.indices.map { _ in [Distance?](repeating: nil, count: 1 << distances.count) }
     func _tsp(position: Int, visited: Int) -> Distance {
-        if visited == (1 << distances.count) - 1 { return distances[position][start] }
+        if let result = tspResults[position][visited] { return result }
+        if visited == (1 << distances.count) - 1 {
+            let result = distances[position][start]
+            tspResults[position][visited] = result
+            return result
+        }
         var minDistance: Distance?
         for next in distances.indices {
             if visited & (1 << next) != 0 { continue }
@@ -9,7 +15,9 @@ func tsp<Distance>(distances: [[Distance]], startedAt start: Int) -> Distance wh
             let distance = distanceToNext + _tsp(position: next, visited: visited | (1 << next))
             minDistance = minDistance.map { Swift.min($0, distance) } ?? distance
         }
-        return minDistance!
+        let result =  minDistance!
+        tspResults[position][visited] = result
+        return result
     }
     return _tsp(position: start, visited: 1 << start)
 }
