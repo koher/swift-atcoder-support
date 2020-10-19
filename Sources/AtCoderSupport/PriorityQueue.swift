abfe4444f60cd77e488b2873f5fd64f3ffe48ee3
Yuta Koshizawa <koher@koherent.org>
2020-10-19 23:12:00 +0900

    Improve performance of `PriorityQueue`

--- Sources/AtCoderSupport/PriorityQueue.swift
+++ Sources/AtCoderSupport/PriorityQueue.swift
@@ -28,38 +28,43 @@ struct PriorityQueue<Element> {
     mutating func append(_ element: Element) {
         var i = elements.count
         elements.append(element)
-        while i > 0 {
-            let parentIndex = (i - 1) >> 1
-            let parent = elements[parentIndex]
-            guard areInIncreasingOrder(element, parent) else { break }
-            elements[parentIndex] = element
-            elements[i] = parent
-            i = parentIndex
+        elements.withUnsafeMutableBufferPointer { elements in
+            while i > 0 {
+                let parentIndex = (i - 1) >> 1
+                let parent = elements[parentIndex]
+                guard areInIncreasingOrder(element, parent) else { break }
+                elements[parentIndex] = element
+                elements[i] = parent
+                i = parentIndex
+            }
         }
     }
     
     mutating func popFirst() -> Element? {
         guard let element = elements.popLast() else { return nil }
         guard let first = elements.first else { return element }
-        elements[0] = element
         
-        var  i = 0
-        while true {
-            var childIndex: Int = (i << 1) + 1
-            guard childIndex < elements.count else { break }
-            var child: Element = elements[childIndex]
-            let rightIndex = childIndex + 1
-            if rightIndex < elements.count {
-                let right = elements[rightIndex]
-                if areInIncreasingOrder(right, child) {
-                    childIndex = rightIndex
-                    child = right
+        elements.withUnsafeMutableBufferPointer { elements in
+            elements[0] = element
+            
+            var  i = 0
+            while true {
+                var childIndex: Int = (i << 1) + 1
+                guard childIndex < elements.count else { break }
+                var child: Element = elements[childIndex]
+                let rightIndex = childIndex + 1
+                if rightIndex < elements.count {
+                    let right = elements[rightIndex]
+                    if areInIncreasingOrder(right, child) {
+                        childIndex = rightIndex
+                        child = right
+                    }
                 }
+                if areInIncreasingOrder(element, child) { break }
+                elements[childIndex] = element
+                elements[i] = child
+                i = childIndex
             }
-            if areInIncreasingOrder(element, child) { break }
-            elements[childIndex] = element
-            elements[i] = child
-            i = childIndex
         }
 
         return first
