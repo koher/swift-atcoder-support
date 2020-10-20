0ad096be35dcd900cc2e0eec924169938e03693d
Yuta Koshizawa <koher@koherent.org>
2020-10-21 00:51:51 +0900

    Implement `values(<,_:)` and `values(<=,_:)`

--- Sources/AtCoderSupport/BinarySearch.swift
+++ Sources/AtCoderSupport/BinarySearch.swift
@@ -1,7 +1,43 @@
+enum LessThanElement {}
+func <(lhs: LessThanElement, rhs: LessThanElement) {}
+extension RandomAccessCollection where Element: Comparable, Index == Int {
+    func values(_: (LessThanElement, LessThanElement) -> (), _ value: Element) -> SubSequence {
+        var low = startIndex - 1
+        var high = endIndex
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if self[mid] >= value {
+                high = mid
+            } else {
+                low = mid
+            }
+        }
+        return self[..<high]
+    }
+}
+
+enum LessThanOrEqualToElement {}
+func <=(lhs: LessThanOrEqualToElement, rhs: LessThanOrEqualToElement) {}
+extension RandomAccessCollection where Element: Comparable, Index == Int {
+    func values(_: (LessThanOrEqualToElement, LessThanOrEqualToElement) -> (), _ value: Element) -> SubSequence {
+        var low = startIndex - 1
+        var high = endIndex
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if self[mid] > value {
+                high = mid
+            } else {
+                low = mid
+            }
+        }
+        return self[..<high]
+    }
+}
+
 enum GreaterThanElement {}
 func >(lhs: GreaterThanElement, rhs: GreaterThanElement) {}
 extension RandomAccessCollection where Element: Comparable, Index == Int {
-    func values(_: (GreaterThanElement, GreaterThanElement) -> (),_ value: Element) -> SubSequence {
+    func values(_: (GreaterThanElement, GreaterThanElement) -> (), _ value: Element) -> SubSequence {
         var low = startIndex - 1
         var high = endIndex
         while high - low > 1 {
@@ -19,7 +55,7 @@ extension RandomAccessCollection where Element: Comparable, Index == Int {
 enum GreaterThanOrEqualToElement {}
 func >=(lhs: GreaterThanOrEqualToElement, rhs: GreaterThanOrEqualToElement) {}
 extension RandomAccessCollection where Element: Comparable, Index == Int {
-    func values(_: (GreaterThanOrEqualToElement, GreaterThanOrEqualToElement) -> (),_ value: Element) -> SubSequence {
+    func values(_: (GreaterThanOrEqualToElement, GreaterThanOrEqualToElement) -> (), _ value: Element) -> SubSequence {
         var low = startIndex - 1
         var high = endIndex
         while high - low > 1 {
--- Tests/AtCoderSupportTests/BinarySearchTests.swift
+++ Tests/AtCoderSupportTests/BinarySearchTests.swift
@@ -2,6 +2,235 @@ import XCTest
 @testable import AtCoderSupport
 
 final class BinarySearchTests: XCTestCase {
+    func testValuesLT() {
+        do {
+            let a = [0, 1, 1, 2, 3, 5, 8]
+            do {
+                let r = a.values(<, -1)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<, 0)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<, 1)
+                XCTAssertEqual(r, [0])
+            }
+            do {
+                let r = a.values(<, 2)
+                XCTAssertEqual(r, [0, 1, 1])
+            }
+            do {
+                let r = a.values(<, 3)
+                XCTAssertEqual(r, [0, 1, 1, 2])
+            }
+            do {
+                let r = a.values(<, 4)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3])
+            }
+            do {
+                let r = a.values(<, 5)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3])
+            }
+            do {
+                let r = a.values(<, 6)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<, 7)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<, 8)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<, 9)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5, 8])
+            }
+            do {
+                let r = a.values(<, 10)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5, 8])
+            }
+        }
+        
+        do { // Collections but `Array`s
+            let a: ArraySlice<Int> = [0, 1, 1, 2, 3, 5, 8, 13][1 ..< 7]
+            do {
+                let r = a.values(<, -1)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<, 0)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<, 1)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<, 2)
+                XCTAssertEqual(r, [1, 1])
+            }
+            do {
+                let r = a.values(<, 3)
+                XCTAssertEqual(r, [1, 1, 2])
+            }
+            do {
+                let r = a.values(<, 4)
+                XCTAssertEqual(r, [1, 1, 2, 3])
+            }
+            do {
+                let r = a.values(<, 5)
+                XCTAssertEqual(r, [1, 1, 2, 3])
+            }
+            do {
+                let r = a.values(<, 6)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<, 7)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<, 8)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<, 9)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5, 8])
+            }
+            do {
+                let r = a.values(<, 10)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5, 8])
+            }
+        }
+        do { // Overflow of `low + high`
+            let a =  (Int.max - 10) ..< Int.max
+            let r = a.values(<, Int.max - 3)
+            XCTAssertEqual(r.count, 7)
+        }
+    }
+    
+    func testValuesLE() {
+        do {
+            let a = [0, 1, 1, 2, 3, 5, 8]
+            do {
+                let r = a.values(<=, -2)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<=, -1)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<=, 0)
+                XCTAssertEqual(r, [0])
+            }
+            do {
+                let r = a.values(<=, 1)
+                XCTAssertEqual(r, [0, 1, 1])
+            }
+            do {
+                let r = a.values(<=, 2)
+                XCTAssertEqual(r, [0, 1, 1, 2])
+            }
+            do {
+                let r = a.values(<=, 3)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3])
+            }
+            do {
+                let r = a.values(<=, 4)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3])
+            }
+            do {
+                let r = a.values(<=, 5)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<=, 6)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<=, 7)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<=, 8)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5, 8])
+            }
+            do {
+                let r = a.values(<=, 9)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5, 8])
+            }
+            do {
+                let r = a.values(<=, 10)
+                XCTAssertEqual(r, [0, 1, 1, 2, 3, 5, 8])
+            }
+        }
+        do { // Collections but `Array`s
+            let a: ArraySlice<Int> = [0, 1, 1, 2, 3, 5, 8, 13][1 ..< 7]
+            do {
+                let r = a.values(<=, -2)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<=, -1)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<=, 0)
+                XCTAssertEqual(r, [])
+            }
+            do {
+                let r = a.values(<=, 1)
+                XCTAssertEqual(r, [1, 1])
+            }
+            do {
+                let r = a.values(<=, 2)
+                XCTAssertEqual(r, [1, 1, 2])
+            }
+            do {
+                let r = a.values(<=, 3)
+                XCTAssertEqual(r, [1, 1, 2, 3])
+            }
+            do {
+                let r = a.values(<=, 4)
+                XCTAssertEqual(r, [1, 1, 2, 3])
+            }
+            do {
+                let r = a.values(<=, 5)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<=, 6)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<=, 7)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5])
+            }
+            do {
+                let r = a.values(<=, 8)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5, 8])
+            }
+            do {
+                let r = a.values(<=, 9)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5, 8])
+            }
+            do {
+                let r = a.values(<=, 10)
+                XCTAssertEqual(r, [1, 1, 2, 3, 5, 8])
+            }
+        }
+        do { // Overflow of `low + high`
+            let a =  (Int.max - 10) ..< Int.max
+            let r = a.values(<=, Int.max - 3)
+            XCTAssertEqual(r.count, 8)
+        }
+    }
+    
     func testValuesGT() {
         do {
             let a = [0, 1, 1, 2, 3, 5, 8]
@@ -224,6 +453,22 @@ final class BinarySearchTests: XCTestCase {
     }
     
     #if !DEBUG
+    func testValuesLTPerformance() {
+        let a = 0 ..< 10_000_000_000_000
+        measure {
+            let r = a.values(<, 9_000_000_000_000)
+            XCTAssertEqual(r.count, 9_000_000_000_000)
+        }
+    }
+    
+    func testValuesLEPerformance() {
+        let a = 0 ..< 10_000_000_000_000
+        measure {
+            let r = a.values(<=, 9_000_000_000_000)
+            XCTAssertEqual(r.count, 9_000_000_000_001)
+        }
+    }
+    
     func testValuesGTPerformance() {
         let a = 0 ..< 10_000_000_000_000
         measure {
