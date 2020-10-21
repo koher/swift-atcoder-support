0452409b585987e5753fc46b9a083aa2ca3dc8aa
taichi <itmtha@gmail.com>
2020-10-21 23:56:50 +0900

    シグネチャの変更

--- Sources/AtCoderSupport/dijkstra.swift
+++ Sources/AtCoderSupport/dijkstra.swift
@@ -1,119 +1,105 @@
-struct PriorityQueue<T> {
-    private var data: [T]
-    private var ordered: (T, T) -> Bool
-
-    public var isEmpty: Bool {
-        return data.isEmpty
-    }
-
-    public var count: Int {
-        return data.count
-    }
-
-    init(_ order: @escaping (T, T) -> Bool) {
-        self.data = []
-        self.ordered = order
-    }
-
-    init<Seq: Sequence>(_ seq: Seq, _ order: @escaping (T, T) -> Bool) where Seq.Element == T {
-        self.data = []
-        self.ordered = order
-
-        for x in seq {
-            push(x)
+struct PriorityQueue<Element> {
+    private var elements: [Element] = []
+    private let areInIncreasingOrder: (Element, Element) -> Bool
+    
+    init<S>(_ elements: S, by areInIncreasingOrder: @escaping (Element, Element) -> Bool) where S: Sequence, S.Element == Element {
+        self.areInIncreasingOrder = areInIncreasingOrder
+        for element in elements {
+            append(element)
         }
     }
-
-    public mutating func pop() -> T? {
-        return data.popLast().map { item in
-            var item = item
-            if !isEmpty {
-                swap(&item, &data[0])
-                siftDown()
+    
+    init(by areInIncreasingOrder: @escaping (Element, Element) -> Bool) {
+        self.init(EmptyCollection(), by: areInIncreasingOrder)
+    }
+    
+    var isEmpty: Bool { elements.isEmpty }
+    var count: Int { elements.count }
+    var first: Element? { elements.first }
+    
+    mutating func append(_ element: Element) {
+        var i = elements.count
+        elements.append(element)
+        elements.withUnsafeMutableBufferPointer { elements in
+            while i > 0 {
+                let parentIndex = (i - 1) >> 1
+                let parent = elements[parentIndex]
+                guard areInIncreasingOrder(element, parent) else { break }
+                elements[parentIndex] = element
+                elements[i] = parent
+                i = parentIndex
             }
-            return item
         }
     }
-
-    public mutating func push(_ item: T) {
-        let oldLen = count
-        data.append(item)
-        siftUp(oldLen)
-    }
-
-    private mutating func siftDown() {
-        var pos = 0
-        let end = count
-
-        data.withUnsafeMutableBufferPointer { bufferPointer in
-            let _data = bufferPointer.baseAddress!
-            swap(&_data[0], &_data[end])
-
-            var child = 2 * pos + 1
-            while child < end {
-                let right = child + 1
-                if right < end && ordered(_data[right], _data[child]) {
-                    child = right
+    
+    mutating func popFirst() -> Element? {
+        guard let element = elements.popLast() else { return nil }
+        guard let first = elements.first else { return element }
+        
+        elements.withUnsafeMutableBufferPointer { elements in
+            elements[0] = element
+            
+            var  i = 0
+            while true {
+                var childIndex: Int = (i << 1) + 1
+                guard childIndex < elements.count else { break }
+                var child: Element = elements[childIndex]
+                let rightIndex = childIndex + 1
+                if rightIndex < elements.count {
+                    let right = elements[rightIndex]
+                    if areInIncreasingOrder(right, child) {
+                        childIndex = rightIndex
+                        child = right
+                    }
                 }
-                swap(&_data[pos], &_data[child])
-                pos = child
-                child = 2 * pos + 1
+                if areInIncreasingOrder(element, child) { break }
+                elements[childIndex] = element
+                elements[i] = child
+                i = childIndex
             }
         }
-        siftUp(pos)
-    }
 
-    private mutating func siftUp(_ pos: Int) {
-        var pos = pos
-        while pos > 0 {
-            let parent = (pos - 1) / 2;
-            if ordered(data[parent], data[pos]) {
-                break
-            }
-            data.swapAt(pos, parent)
-            pos = parent
-        }
+        return first
     }
 }
-
-extension PriorityQueue: Sequence, IteratorProtocol {
-    mutating func next() -> T? {
-        return pop()
+extension PriorityQueue where Element: Comparable {
+    init<S>(_ elements: S) where S: Sequence, S.Element == Element {
+        self.init(elements, by: <)
     }
+    
+    init() {
+        self.init(by: <)
+    }
+    
 }
-
-// 第一引数に隣接リスト表現(重み付きver.)のグラフを渡す　graph[i] = [[iに隣接する頂点j, 辺の重み], …, …]
-// 第二引数に始点を渡す
-func dijkstra(graph:[[[Int]]],start:Int)->[Int]{
+func dijkstra<Distance>(graph: [[(index: Int, distance: Distance)]], startedAt start: Int) -> [Int] where Distance: Comparable, Distance: AdditiveArithmetic{
     let INF = Int.max
-    var dist = Array(repeating: INF, count: graph.count) // 各ノードの頂点からの最短距離
+    var dist = Array(repeating: INF, count: graph.count)
     dist[start] = 0
-    var used = Array(repeating:false,count:graph.count)
-    var q = PriorityQueue<(Int, Int)>(<=) // 「仮の最短距離, 頂点」が小さい順に並ぶ
-    q.push((0,start)) // ノードの情報(始点からの距離,ノード名)
+    var used = Array(repeating:false,count: graph.count)
+    var q = PriorityQueue<(Int, Int)>(by:<=)
+    q.append((0,start))
     while !q.isEmpty{
-        //始点からの距離が一番短いノードの情報を取得
-        let label = q.pop()!
+        let label = q.popFirst()!
         let prove_cost = label.0
         let src = label.1 
-
+        
         if(used[src]){
             continue
         }
-
+        
         used[src] = true
         dist[src] = prove_cost
-
+        
         for edge in graph[src]{
-            let cost = edge[0]
-            let dest = edge[1]
-            if dist[dest] > dist[src]+cost{ // 最短距離候補なら プライオリティーキュー に追加
+            let cost = edge.0
+            let dest = edge.1 as! Int
+            if dist[dest] > dist[src]+cost{
                 dist[dest] = dist[src]+cost
-                q.push((dist[dest],dest))
+                q.append((dist[dest],dest))
             }
-
+            
         }
     }
-    // 始点から各頂点までの最小コストをまとめた配列を返す
     return dist
 }
