02d2fb83ce65970f6cfcd407ecc82bc1cfc81a18
Yuta Koshizawa <koher@koherent.org>
2020-10-26 01:24:07 +0900

    Improve performance of `dfs`

--- Sources/AtCoderSupport/DFS.swift
+++ Sources/AtCoderSupport/DFS.swift
@@ -1,11 +1,11 @@
 func dfs(edges: [[Int]], startedAt start: Int, _ body: (Int) -> Void) {
     precondition(edges.indices.contains(start), "`start` index is out of bounds: \(start)")
-    var visited: Set<Int> = []
+    var isVisited: [Bool] = .init(repeating: false, count: edges.count)
     var destinations: [Int] = [start]
     while let current = destinations.popLast() {
-        if visited.contains(current) { continue }
+        if isVisited[current] { continue }
         body(current)
-        visited.insert(current)
+        isVisited[current] = true
         for destination in edges[current].reversed() {
             destinations.append(destination)
         }
@@ -14,12 +14,12 @@ func dfs(edges: [[Int]], startedAt start: Int, _ body: (Int) -> Void) {
 
 func dfs(edges: [[Int]], startedAt start: Int, _ body: (_ current: Int, _ prev: Int?) -> Void) {
     precondition(edges.indices.contains(start), "`start` index is out of bounds: \(start)")
-    var visited: Set<Int> = []
+    var isVisited: [Bool] = .init(repeating: false, count: edges.count)
     var destinations: [(current: Int, prev: Int?)] = [(start, nil)]
     while let (current, prev) = destinations.popLast() {
-        if visited.contains(current) { continue }
+        if isVisited[current] { continue }
         body(current, prev)
-        visited.insert(current)
+        isVisited[current] = true
         for destination in edges[current].reversed() {
             destinations.append((destination, current))
         }
@@ -28,12 +28,12 @@ func dfs(edges: [[Int]], startedAt start: Int, _ body: (_ current: Int, _ prev: 
 
 func dfs(edges: [[Int]], startedAt start: Int, _ body: (_ current: Int, _ prev: Int?, _ depth: Int) -> Void) {
     precondition(edges.indices.contains(start), "`start` index is out of bounds: \(start)")
-    var visited: Set<Int> = []
+    var isVisited: [Bool] = .init(repeating: false, count: edges.count)
     var destinations: [(current: Int, prev: Int?, depth: Int)] = [(start, nil, 0)]
     while let (current, prev, depth) = destinations.popLast() {
-        if visited.contains(current) { continue }
+        if isVisited[current] { continue }
         body(current, prev, depth)
-        visited.insert(current)
+        isVisited[current] = true
         for destination in edges[current].reversed() {
             destinations.append((destination, current, depth + 1))
         }
