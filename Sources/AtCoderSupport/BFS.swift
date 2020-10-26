683478ef3a48bdc2fec4735bf92e579a442797fe
Yuta Koshizawa <koher@koherent.org>
2020-10-26 23:24:24 +0900

    Improve performance of `bfs`

--- Sources/AtCoderSupport/BFS.swift
+++ Sources/AtCoderSupport/BFS.swift
@@ -1,13 +1,13 @@
 func bfs(edges: [[Int]], startedAt start: Int, _ operation: (Int) -> Void) {
     precondition(edges.indices.contains(start), "`start` index is out of bounds: \(start)")
-    var visited: Set<Int> = []
+    var isVisited: [Bool] = .init(repeating: false, count: edges.count)
     var nexts: [Int] = [start]
     while !nexts.isEmpty {
         var destinations: [Int] = []
         for current in nexts {
-            if visited.contains(current) { continue }
+            if isVisited[current] { continue }
             operation(current)
-            visited.insert(current)
+            isVisited[current] = true
             for destination in edges[current] {
                 destinations.append(destination)
             }
@@ -18,14 +18,14 @@ func bfs(edges: [[Int]], startedAt start: Int, _ operation: (Int) -> Void) {
 
 func bfs(edges: [[Int]], startedAt start: Int, _ operation: (_ current: Int, _ prev: Int?) -> Void) {
     precondition(edges.indices.contains(start), "`start` index is out of bounds: \(start)")
-    var visited: Set<Int> = []
+    var isVisited: [Bool] = .init(repeating: false, count: edges.count)
     var nexts: [(current: Int, prev: Int?)] = [(start, nil)]
     while !nexts.isEmpty {
         var destinations: [(current: Int, prev: Int?)] = []
         for (current, prev) in nexts {
-            if visited.contains(current) { continue }
+            if isVisited[current] { continue }
             operation(current, prev)
-            visited.insert(current)
+            isVisited[current] = true
             for destination in edges[current] {
                 destinations.append((destination, current))
             }
@@ -36,14 +36,14 @@ func bfs(edges: [[Int]], startedAt start: Int, _ operation: (_ current: Int, _ p
 
 func bfs(edges: [[Int]], startedAt start: Int, _ operation: (_ current: Int, _ prev: Int?, _ depth: Int) -> Void) {
     precondition(edges.indices.contains(start), "`start` index is out of bounds: \(start)")
-    var visited: Set<Int> = []
+    var isVisited: [Bool] = .init(repeating: false, count: edges.count)
     var nexts: [(current: Int, prev: Int?, depth: Int)] = [(start, nil, 0)]
     while !nexts.isEmpty {
         var destinations: [(current: Int, prev: Int?, depth: Int)] = []
         for (current, prev, depth) in nexts {
-            if visited.contains(current) { continue }
+            if isVisited[current] { continue }
             operation(current, prev, depth)
-            visited.insert(current)
+            isVisited[current] = true
             for destination in edges[current] {
                 destinations.append((destination, current, depth + 1))
             }
