f28cd3914b0ddbe3a5663facd40b1d96dc5512bd
Yuta Koshizawa <koher@koherent.org>
2020-11-01 02:25:11 +0900

    Add a test case for large number and modulus

--- Tests/AtCoderSupportTests/RangeSumTests.swift
+++ Tests/AtCoderSupportTests/RangeSumTests.swift
@@ -11,5 +11,6 @@ final class RangeSumTests: XCTestCase {
         XCTAssertEqual((-10 ... 100).sum(modulus: 13), 4995 % 13)
         XCTAssertEqual((-10 ... -1).sum(modulus: 13), -55 % 13 + 13)
         XCTAssertEqual((-100 ... 10).sum(modulus: 13), -4995 % 13 + 13)
+        XCTAssertEqual((1 ... 1000000000).sum(modulus: 998244353), (1000000000 * 1000000001 / 2) % 998244353)
     }
 }
