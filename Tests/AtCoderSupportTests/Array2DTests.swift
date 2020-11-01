120754d25e8aed7d46ebb1814c8bbc9948670c41
Yuta Koshizawa <koher@koherent.org>
2020-11-01 14:17:52 +0900

    Implement `Array2D`

---
+++ Tests/AtCoderSupportTests/Array2DTests.swift
@@ -0,0 +1,91 @@
+import XCTest
+@testable import AtCoderSupport
+
+final class Array2DTests: XCTestCase {
+    func testInit() {
+        do {
+            let a: Array2D<Int> = .init(width: 3, height: 2, element: 42)
+            
+            XCTAssertEqual(a[0, 0], 42)
+            XCTAssertEqual(a[1, 0], 42)
+            XCTAssertEqual(a[2, 0], 42)
+
+            XCTAssertEqual(a[0, 1], 42)
+            XCTAssertEqual(a[1, 1], 42)
+            XCTAssertEqual(a[2, 1], 42)
+        }
+    }
+    
+    func testCount() {
+        do {
+            let a: Array2D<Int> = .init(width: 4, height: 3, element: 42)
+            let r = a.count
+            XCTAssertEqual(r, 12)
+        }
+    }
+    
+    func testSubscriptGet() {
+        do {
+            let a: Array2D<Int> = .init(width: 4, height: 3, elements: [
+                1, 2, 3, 4,
+                5, 6, 7, 8,
+                9, 10, 11, 12,
+            ])
+            
+            XCTAssertEqual(a[0, 0], 1)
+            XCTAssertEqual(a[1, 0], 2)
+            XCTAssertEqual(a[2, 0], 3)
+            XCTAssertEqual(a[3, 0], 4)
+            
+            XCTAssertEqual(a[0, 1], 5)
+            XCTAssertEqual(a[1, 1], 6)
+            XCTAssertEqual(a[2, 1], 7)
+            XCTAssertEqual(a[3, 1], 8)
+            
+            XCTAssertEqual(a[0, 2], 9)
+            XCTAssertEqual(a[1, 2], 10)
+            XCTAssertEqual(a[2, 2], 11)
+            XCTAssertEqual(a[3, 2], 12)
+        }
+    }
+    
+    func testSubscriptSet() {
+        do {
+            var a: Array2D<Int> = .init(width: 4, height: 3, elements: [
+                1, 2, 3, 4,
+                5, 6, 7, 8,
+                9, 10, 11, 12,
+            ])
+            
+            a[0, 0] = 100
+            a[1, 0] = 200
+            a[2, 0] = 300
+            a[3, 0] = 400
+            
+            a[0, 1] = 500
+            a[1, 1] = 600
+            a[2, 1] = 700
+            a[3, 1] = 800
+            
+            a[0, 2] = 900
+            a[1, 2] = 1000
+            a[2, 2] = 1100
+            a[3, 2] = 1200
+            
+            XCTAssertEqual(a[0, 0], 100)
+            XCTAssertEqual(a[1, 0], 200)
+            XCTAssertEqual(a[2, 0], 300)
+            XCTAssertEqual(a[3, 0], 400)
+            
+            XCTAssertEqual(a[0, 1], 500)
+            XCTAssertEqual(a[1, 1], 600)
+            XCTAssertEqual(a[2, 1], 700)
+            XCTAssertEqual(a[3, 1], 800)
+            
+            XCTAssertEqual(a[0, 2], 900)
+            XCTAssertEqual(a[1, 2], 1000)
+            XCTAssertEqual(a[2, 2], 1100)
+            XCTAssertEqual(a[3, 2], 1200)
+        }
+    }
+}
