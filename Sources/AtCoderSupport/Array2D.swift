b25a1d818153bcd5d5ed7cf5aaffe0f61b1361fa
Yuta Koshizawa <koher@koherent.org>
2020-11-16 01:31:30 +0900

    Implement `Array2D.outside`

--- Sources/AtCoderSupport/Array2D.swift
+++ Sources/AtCoderSupport/Array2D.swift
@@ -1,32 +1,75 @@
-struct Array2D<Element>: Sequence {
+struct Array2D<Element>: Sequence, CustomStringConvertible {
     let width: Int
     let height: Int
     private(set) var elements: [Element]
-    init(width: Int, height: Int, elements: [Element]) {
+    let outside: Element?
+    init(width: Int, height: Int, elements: [Element], outside: Element? = nil) {
         precondition(elements.count == width * height)
         self.width = width
         self.height = height
         self.elements = elements
+        self.outside = outside
     }
-    init(width: Int, height: Int, element: Element) {
-        self.init(width: width, height: height, elements: [Element](repeating: element, count: width * height))
+    init(width: Int, height: Int, element: Element, outside: Element? = nil) {
+        self.init(width: width, height: height, elements: [Element](repeating: element, count: width * height), outside: outside)
     }
     var count: Int { width * height }
     var xRange: Range<Int> { 0 ..< width }
     var yRange: Range<Int> { 0 ..< height }
-    private func indexAt(x: Int, y: Int) -> Int {
-        precondition(xRange.contains(x))
-        precondition(yRange.contains(y))
+    private func indexAt(x: Int, y: Int) -> Int? {
+        guard xRange.contains(x) else { return nil }
+        guard yRange.contains(y) else { return nil }
         return y * width + x
     }
     subscript(x: Int, y: Int) -> Element {
-        get { elements[indexAt(x: x, y: y)] }
-        set { elements[indexAt(x: x, y: y)] = newValue }
+        get {
+            guard let i = indexAt(x: x, y: y) else { return outside! }
+            return elements[i]
+        }
+        set {
+            guard let i = indexAt(x: x, y: y) else {
+                precondition(outside != nil)
+                return
+            }
+            elements[i] = newValue
+        }
+    }
+    subscript(position: (Int, Int)) -> Element {
+        get { self[position.0, position.1] }
+        set { self[position.0, position.1] = newValue }
     }
     func makeIterator() -> IndexingIterator<[Element]> {
         elements.makeIterator()
     }
     func map<T>(_ transform: (Element) throws -> T) rethrows -> Array2D<T> {
         try Array2D<T>(width: width, height: height, elements: elements.map(transform))
+    }
+    var description: String {
+        var result: String = ""
+        for y in yRange {
+            for x in xRange {
+                if x > 0 {
+                    result.append(" ")
+                }
+                result.append("\(self[x, y])")
+            }
+            result.append("\n")
+        }
+        return result
+    }
+}
+extension Array2D where Element: CustomStringConvertible {
+    var description: String {
+        var result: String = ""
+        for y in yRange {
+            for x in xRange {
+                if x > 0 {
+                    result.append(" ")
+                }
+                result.append(self[x, y].description)
+            }
+            result.append("\n")
+        }
+        return result
     }
 }
--- Tests/AtCoderSupportTests/Array2DTests.swift
+++ Tests/AtCoderSupportTests/Array2DTests.swift
@@ -47,6 +47,33 @@ final class Array2DTests: XCTestCase {
             XCTAssertEqual(a[2, 2], 11)
             XCTAssertEqual(a[3, 2], 12)
         }
+        do {
+            let a: Array2D<Int> = .init(width: 4, height: 3, elements: [
+                1, 2, 3, 4,
+                5, 6, 7, 8,
+                9, 10, 11, 12,
+            ], outside: -1)
+            
+            XCTAssertEqual(a[0, 0], 1)
+            XCTAssertEqual(a[1, 0], 2)
+            XCTAssertEqual(a[2, 0], 3)
+            XCTAssertEqual(a[3, 0], 4)
+            
+            XCTAssertEqual(a[0, 1], 5)
+            XCTAssertEqual(a[1, 1], 6)
+            XCTAssertEqual(a[2, 1], 7)
+            XCTAssertEqual(a[3, 1], 8)
+            
+            XCTAssertEqual(a[0, 2], 9)
+            XCTAssertEqual(a[1, 2], 10)
+            XCTAssertEqual(a[2, 2], 11)
+            XCTAssertEqual(a[3, 2], 12)
+            
+            XCTAssertEqual(a[-1, -1], -1)
+            XCTAssertEqual(a[4, -1], -1)
+            XCTAssertEqual(a[-1, 3], -1)
+            XCTAssertEqual(a[4, 3], -1)
+        }
     }
     
     func testSubscriptSet() {
@@ -55,7 +82,7 @@ final class Array2DTests: XCTestCase {
                 1, 2, 3, 4,
                 5, 6, 7, 8,
                 9, 10, 11, 12,
-            ])
+            ], outside: -1)
             
             a[0, 0] = 100
             a[1, 0] = 200
@@ -72,6 +99,11 @@ final class Array2DTests: XCTestCase {
             a[2, 2] = 1100
             a[3, 2] = 1200
             
+            a[-1, -1] = -100
+            a[4, -1] = -100
+            a[-1, 3] = -100
+            a[4, 3] = -100
+
             XCTAssertEqual(a[0, 0], 100)
             XCTAssertEqual(a[1, 0], 200)
             XCTAssertEqual(a[2, 0], 300)
@@ -86,6 +118,11 @@ final class Array2DTests: XCTestCase {
             XCTAssertEqual(a[1, 2], 1000)
             XCTAssertEqual(a[2, 2], 1100)
             XCTAssertEqual(a[3, 2], 1200)
+            
+            XCTAssertEqual(a[-1, -1], -1)
+            XCTAssertEqual(a[4, -1], -1)
+            XCTAssertEqual(a[-1, 3], -1)
+            XCTAssertEqual(a[4, 3], -1)
         }
     }
 }
