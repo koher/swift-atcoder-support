16826ae232c1a498b828efeda3bd522b7d924b31
Yuta Koshizawa <koher@koherent.org>
2021-02-21 02:40:53 +0900

    Deintegrate binary search methods for each operators
    To keep each method simple.

--- Sources/AtCoderSupport/BinarySearch.swift
+++ Sources/AtCoderSupport/BinarySearch.swift
@@ -1,60 +1,131 @@
+enum LessThanElement {}
+func <(lhs: LessThanElement, rhs: LessThanElement) {}
 extension RandomAccessCollection where Element: Comparable, Index == Int {
-    func values(_ op: (Element, Element) -> Bool, _ value: Element) -> SubSequence {
-        if self.isEmpty { return self[...] }
-        if self.count == 1 {
-            if op(self.first!, value) {
-                return self[...]
-            } else {
-                return self[startIndex ..< startIndex]
-            }
-        }
-        let isLess = op(self.first!, self.last!)
-        let op = isLess ? op : { !op($0, $1) }
-        
+    func values(_: (LessThanElement, LessThanElement) -> (), _ value: Element) -> SubSequence {
         var low = startIndex - 1
         var high = endIndex
         while high - low > 1 {
             let mid = low + (high - low) / 2
-            if op(self[mid], value) {
+            if self[mid] < value {
                 low = mid
             } else {
                 high = mid
             }
         }
-        if isLess {
-            return self[..<high]
-        } else {
-            return self[high...]
+        return self[..<high]
+    }
+}
+extension RandomAccessCollection where Index == Int {
+    func values(_: (LessThanElement, LessThanElement) -> (), _ predicate: (Element) throws -> Bool) rethrows -> SubSequence {
+        var low = startIndex - 1
+        var high = endIndex
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if try predicate(self[mid]) {
+                low = mid
+            } else {
+                high = mid
+            }
         }
+        return self[..<high]
     }
 }
 
-extension RandomAccessCollection where Index == Int {
-    func values(_ op: (Int, Int) -> Bool, _ predicate: (Element) throws -> Bool) rethrows -> SubSequence {
-        let isLess = op(1, 2)
-        
+enum LessThanOrEqualToElement {}
+func <=(lhs: LessThanOrEqualToElement, rhs: LessThanOrEqualToElement) {}
+extension RandomAccessCollection where Element: Comparable, Index == Int {
+    func values(_: (LessThanOrEqualToElement, LessThanOrEqualToElement) -> (), _ value: Element) -> SubSequence {
         var low = startIndex - 1
         var high = endIndex
-        if isLess {
-            while high - low > 1 {
-                let mid = low + (high - low) / 2
-                if try predicate(self[mid]) {
-                    low = mid
-                } else {
-                    high = mid
-                }
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if self[mid] <= value {
+                low = mid
+            } else {
+                high = mid
             }
-            return self[..<high]
-        } else {
-            while high - low > 1 {
-                let mid = low + (high - low) / 2
-                if try predicate(self[mid]) {
-                    high = mid
-                } else {
-                    low = mid
-                }
-            }
-            return self[high...]
         }
+        return self[..<high]
+    }
+}
+extension RandomAccessCollection where Index == Int {
+    func values(_: (LessThanOrEqualToElement, LessThanOrEqualToElement) -> (), _ predicate: (Element) throws -> Bool) rethrows -> SubSequence {
+        var low = startIndex - 1
+        var high = endIndex
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if try predicate(self[mid]) {
+                low = mid
+            } else {
+                high = mid
+            }
+        }
+        return self[..<high]
+    }
+}
+
+enum GreaterThanElement {}
+func >(lhs: GreaterThanElement, rhs: GreaterThanElement) {}
+extension RandomAccessCollection where Element: Comparable, Index == Int {
+    func values(_: (GreaterThanElement, GreaterThanElement) -> (), _ value: Element) -> SubSequence {
+        var low = startIndex - 1
+        var high = endIndex
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if self[mid] > value {
+                high = mid
+            } else {
+                low = mid
+            }
+        }
+        return self[high...]
+    }
+}
+extension RandomAccessCollection where Index == Int {
+    func values(_: (GreaterThanElement, GreaterThanElement) -> (), _ predicate: (Element) throws -> Bool) rethrows -> SubSequence {
+        var low = startIndex - 1
+        var high = endIndex
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if try predicate(self[mid]) {
+                high = mid
+            } else {
+                low = mid
+            }
+        }
+        return self[high...]
+    }
+}
+
+enum GreaterThanOrEqualToElement {}
+func >=(lhs: GreaterThanOrEqualToElement, rhs: GreaterThanOrEqualToElement) {}
+extension RandomAccessCollection where Element: Comparable, Index == Int {
+    func values(_: (GreaterThanOrEqualToElement, GreaterThanOrEqualToElement) -> (), _ value: Element) -> SubSequence {
+        var low = startIndex - 1
+        var high = endIndex
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if self[mid] >= value {
+                high = mid
+            } else {
+                low = mid
+            }
+        }
+        return self[high...]
+    }
+}
+extension RandomAccessCollection where Index == Int {
+    func values(_: (GreaterThanOrEqualToElement, GreaterThanOrEqualToElement) -> (), _ predicate: (Element) throws -> Bool) rethrows -> SubSequence {
+        var low = startIndex - 1
+        var high = endIndex
+        while high - low > 1 {
+            let mid = low + (high - low) / 2
+            if try predicate(self[mid]) {
+                high = mid
+            } else {
+                low = mid
+            }
+        }
+        return self[high...]
     }
 }
